f9dfd8afe7e33317c017a7ec78af5dd79556d70d
Avinash Fernando <avinashfernando452@gmail.com>
2025-06-10 23:46:35 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,10 @@
 <?php
-// Step 5a: Logout System
-// Save this as: logout.php
 
 session_start();
 
-// Destroy all session data
 session_unset();
 session_destroy();
 
-// Redirect to homepage with logout message
 header('Location: index.php?logout=success');
 exit();
 ?>
